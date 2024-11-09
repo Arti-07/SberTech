bb9a3984c910743c86b7ef8ca114d911dc3ac085
Arti-07 <aginiatulllin@gmail.com>
2024-11-10 00:12:21 +0300

    Initial commit

---
+++ bro.config.js
@@ -0,0 +1,22 @@
+const pkg = require("./package");
+
+module.exports = {
+  apiPath: "stubs/api",
+  webpackConfig: {
+    output: {
+      publicPath: `/static/${pkg.name}/${process.env.VERSION || pkg.version}/`,
+    },
+  },
+  /* use https://admin.bro-js.ru/ to create config, navigations and features */
+  navigations: {
+    "sbertech.main": "/sbertech",
+  },
+  features: {
+    "sbertech": {
+      // add your features here in the format [featureName]: { value: string }
+    },
+  },
+  config: {
+    "sbertech.api": "/api",
+  },
+};
