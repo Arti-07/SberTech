f32b2937a0e24f4e22bcda38a10c2761f21b7b60
ALMAZKHAYBULLIN <almaz.khaybullin@mail.ru>
2024-11-24 00:36:20 +0300

    [wip][account_page] add account page (SCRUM-11)

--- src/container/list/index.tsx
+++ src/container/list/index.tsx
@@ -1,13 +1,7 @@
 import React from 'react';
-import Favorites from "./favorites";
 
 const ListPage = (): React.ReactElement => {
-    return (
-        <div style={{ padding: '20px' }}>
-            <h1>Favorites</h1>
-            <Favorites />
-        </div>
-    );
+    return <h1>List Page</h1>;
 };
 
 export default ListPage;
