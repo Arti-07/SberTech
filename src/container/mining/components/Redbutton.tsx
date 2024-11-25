1e48ea9132d2198d315de1d34a23b57f750fcdd2
Arti-07 <aginiatulllin@gmail.com>
2024-11-25 23:50:43 +0300

    Убрал использование стилей в кнопке

--- src/container/mining/components/Redbutton.tsx
+++ src/container/mining/components/Redbutton.tsx
@@ -1,12 +1,9 @@
 import React from 'react';
-import { useState } from 'react'; //useState - это хук, который позволяет управлять состоянием компонента.
+import {useState} from 'react';
 
-import './Counter.css';
 
 export default function Redbutton() {
     const [count, setCount] = useState(0);
-    //count - состояние, которое изначально равно нулю.
-    //setCount – функция, которая обновляет значение состояния count.
 
     const handleClick = () => {
         setCount(count + 1);
