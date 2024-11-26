bc842131f7ebb440193a79a6f971b61201bd336c
Leonid Kurlov <KurlovL@mail.ru>
2024-11-26 23:44:35 +0300

    Добавил первый прототип анимированной кнопки

--- src/container/mining/index.tsx
+++ src/container/mining/index.tsx
@@ -1,8 +1,32 @@
 import React from 'react';
 import Redbutton from './components/Redbutton';
 
+import "./styles.css";
+import { motion } from 'framer-motion'
+
 const MiningPage = (): React.ReactElement => {
-    return <Redbutton />;
+    return (
+        <>
+            {/* <Redbutton /> */}
+
+            {/* <motion.div className="box" animate={{ x: 100 }} /> */}
+            <motion.div className="box" 
+                animate={{
+                    scale: 0.1,
+                    transition: { 
+                        duration: 2
+                    }
+                }} 
+            />
+
+            <motion.div
+                className="box"
+                whileHover={{ scale: 1.1 }}
+                whileTap={{ scale: 0.9 }}
+                transition={{ type: "spring", stiffness: 800, damping: 10 }}
+            />
+        </>
+    );
 };
 
 export default MiningPage;
