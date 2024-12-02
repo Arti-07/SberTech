8007c00f67933b190ea86ea530269c5104960d9b
ALMAZKHAYBULLIN <almaz.khaybullin@mail.ru>
2024-12-02 23:09:55 +0300

    Added login and register pages

--- src/container/main/router.tsx
+++ src/container/main/router.tsx
@@ -4,6 +4,8 @@ import ListPage from '../list';
 import DetailPage from '../detail';
 import MiningPage from "../mining";
 import AccountPage from "../account";
+import SigninPage from "../signin";
+import SignupPage from "../signup";
 import {getNavigationsValue} from '@brojs/cli';
 import Layout from './components/layout';
 
@@ -15,6 +17,14 @@ export const router = createBrowserRouter([
             {
                 path: getNavigationsValue('smartini_crypto.account'),
                 element:<AccountPage/>
+            },
+            {
+                path: getNavigationsValue('smartini_crypto.signin'),
+                element:<SigninPage/>
+            },
+            {
+                path: getNavigationsValue('smartini_crypto.signup'),
+                element:<SignupPage/>
             },
             {
                 path: getNavigationsValue('smartini_crypto.main'),
