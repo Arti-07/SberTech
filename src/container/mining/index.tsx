3d92a37c1258ecfe6723d599e2e2bed0e575b91d
Leonid Kurlov <KurlovL@mail.ru>
2024-12-12 00:14:53 +0300

    [fix] исправлены названия файлов

--- src/container/mining/index.tsx
+++ src/container/mining/index.tsx
@@ -1,13 +1,23 @@
 import  React from 'react';
 import { useState, useEffect } from 'react';
 
-import Babah from './components/Babah';
-import Speedometer from './components/Speedometer';
-import MotioButton from './components/MotionButton';
+import Babah from './components/babah';
+import Speedometer from './components/speedometer';
+import MotioButton from './components/motionButton';
 
-import { containerCenterStyle } from './components/ContainerCenter.style'
+import { containerCenterStyle } from './components/containerCenter.style'
 import { Global, css } from '@emotion/react'
+import styled from "@emotion/styled";
 
+const AccountTextStyled = styled.h3`
+    min-width: 200px;
+    font-size: 2.25rem;
+    font-weight: 400;
+    text-align: center;
+    margin-top: 2%;
+    margin-bottom: 2%;
+    padding: 0;
+`;
 
 const MiningPage = (): React.ReactElement => {
 
@@ -16,9 +26,6 @@ const MiningPage = (): React.ReactElement => {
     const [progress, setProgress] = useState(0)
     const [isVisible, setIsVisible] = useState(0)
     
-    // create a state to store the speed
-    const [speed, setSpeed] = useState(0)
-
     useEffect(() => {        
         const interval = setInterval(() => {
             setProgress(p => Math.max(p - 0.007, 0) );            
@@ -30,18 +37,17 @@ const MiningPage = (): React.ReactElement => {
     
     return (
         <>
-            <div className={containerCenterStyle}>
-                <h1>Счет: {countMining}</h1>                
-            </div>            
             <Global
             styles={css`
                 body {
                     position: fixed;
                     width: 100%;
-                    overflow-y: scroll;
                 }
             `}
             />            
+            <div className={containerCenterStyle}>
+                <AccountTextStyled> Счет: {countMining}</AccountTextStyled>                
+            </div>
             <Speedometer turn={progress} />
             <MotioButton
             progress={progress}
@@ -50,14 +56,14 @@ const MiningPage = (): React.ReactElement => {
             stepMining={stepMining}
             setCountMining={setCountMining} 
             />
-            <div className={containerCenterStyle}>
             {isVisible ? (
+                <div className={containerCenterStyle}>
                     <Babah/>
+                </div>
                 ) : (
                     <></>
                 )
             }
-            </div>
         </>
     );
 };
