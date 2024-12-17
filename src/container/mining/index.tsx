52cdda22a39fc6a013fd83259db3f32f2c5d019b
Leonid Kurlov <KurlovL@mail.ru>
2024-12-17 23:20:03 +0300

    [fix] reposition coin and babah

--- src/container/mining/index.tsx
+++ src/container/mining/index.tsx
@@ -1,19 +1,18 @@
-import  React from 'react';
+import React from 'react';
 import { useState, useEffect, useRef } from 'react';
 
 import Babah from './components/babah';
 import Speedometer from './components/speedometer';
 import MotioButton from './components/motionButton';
 
-import { containerCenterStyle } from './components/containerCenter.style'
-import { Global } from '@emotion/react'
-import { css } from '@emotion/css'
-import styled from "@emotion/styled";
+import { containerCenterStyle } from './components/containerCenter.style';
+import { Global } from '@emotion/react';
+import { css } from '@emotion/css';
+import styled from '@emotion/styled';
 
-import confettiAnimation from '../../assets/lotties/confetti.json'
-import coinAnimation from '../../assets/lotties/coin.json'
-import Lottie from 'react-lottie'
-
+import confettiAnimation from '../../assets/lotties/confetti.json';
+import coinAnimation from '../../assets/lotties/coin.json';
+import Lottie from 'react-lottie';
 
 const AccountTextStyled = styled.h3`
     min-width: 250px;
@@ -33,117 +32,112 @@ const confettiStyle = css`
     transform: translate(-50%, 0%);
     z-index: 5;
     pointer-events: none;
-`
+`;
 
 const MiningContainer = styled.div`
     position: relative;
-`
+`;
 
 const AccountStyle = styled.div`
     outline: 1rem solid;
-`
+`;
 
 const ContainerAccount = styled.div`
-.container {
-display: flex;
-justify-content: center;
-align-items: center;
-}
+    .container {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
 
-.card {
-min-width: 400px;
-width: 400px;
-height: 60px;
-background: linear-gradient(to right, lightgray, darkblue); /* Градиент */
-border-radius: 20px; /* Закругленные края */
-box-shadow: 0 4px 8px rgba(0, 0, 0, 0.25);
-display: flex;
-overflow: hidden;
-pointer-events: none;
-user-select: none;
-justify-content: center;
-}
+    .card {
+        min-width: 400px;
+        width: 400px;
+        height: 60px;
+        background: linear-gradient(to right, lightgray, darkblue); /* Градиент */
+        border-radius: 20px; /* Закругленные края */
+        box-shadow: 0 4px 8px rgba(0, 0, 0, 0.25);
+        display: flex;
+        overflow: hidden;
+        pointer-events: none;
+        user-select: none;
+        justify-content: center;
+    }
 
-.card img {
-width: 40%;
-object-fit: cover;
-}
+    .card img {
+        width: 40%;
+        object-fit: cover;
+    }
 
-.account {
-display: flex;
-flex-direction: column;
-justify-content: center;
-}
+    .account {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+    }
 
+    .account-button {
+        margin-left: 16px;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+    }
 
-.account-button {
-margin-left: 16px;
-display: flex;
-flex-direction: column;
-justify-content: center;
-}
+    .account-button button {
+        padding: 8px 16px;
+        background-color: white;
+        border: none;
+        border-radius: 6px;
+        cursor: pointer;
+        transition: all 0.3s ease-in-out;
+        transform: scale(1);
+    }
 
-.account-button button {
-padding: 8px 16px;
-background-color: white;
-border: none;
-border-radius: 6px;
-cursor: pointer;
-transition: all 0.3s ease-in-out;
-transform: scale(1);
-}
+    .account-button button:hover {
+        transform: scale(1.1);
+    }
 
-.account-button button:hover {
-    transform: scale(1.1);
-}
-
-.coin{
-    z-index: 2;
-}
-`
-
+    .coin {
+        z-index: 2;
+    }
+`;
 
 const MiningPage = (): React.ReactElement => {
-
-    
     const lottieRef = useRef(null);
 
-    const [countMining, setCountMining] = useState(0)
-    const [stepMining, setStepMining] = useState(1)
-    const [progress, setProgress] = useState(0)
-    const [isVisible, setIsVisible] = useState(0)
+    const [countMining, setCountMining] = useState(0);
+    const [stepMining, setStepMining] = useState(1);
+    const [progress, setProgress] = useState(0);
+    const [isVisible, setIsVisible] = useState(0);
     const stepDecrease = 0.007;
     const stepIncrease = 0.1;
     const maxProgress = 1.5;
     const timeDecrease = 10;
     const minStepMining = 1;
     const maxStepMining = 5;
-    
-    useEffect(() => {        
+
+    useEffect(() => {
         const interval = setInterval(() => {
-            setProgress(p => Math.max(p - stepDecrease, 0) );            
-            setIsVisible(Number(progress > 1) );
+            setProgress((p) => Math.max(p - stepDecrease, 0));
+            setIsVisible(Number(progress > 1));
             setStepMining(isVisible ? maxStepMining : minStepMining);
-        }, timeDecrease); 
+        }, timeDecrease);
         return () => clearInterval(interval);
     }, [progress, stepMining, isVisible]);
-
 
     const defaultOptions = {
         loop: true,
         autoplay: true,
         animationData: confettiAnimation,
         rendererSettings: {
-            preserveAspectRatio: "xMidYMid slice"
+            preserveAspectRatio: 'xMidYMid slice'
         }
     };
 
-    const defaultOptions2 = {
+    const defaultOptionsCoin = {
         loop: true,
         autoplay: true,
         animationData: coinAnimation,
         rendererSettings: {
-            preserveAspectRatio: "xMidYMid slice"
+            preserveAspectRatio: 'xMidYMid slice'
         }
     };
 
@@ -152,7 +146,7 @@ const MiningPage = (): React.ReactElement => {
             lottieRef.current.setSpeed(1); // Продолжить анимацию с нормальной скоростью
         }
     };
-        
+
     return (
         <>
             {/* <Global
@@ -165,69 +159,44 @@ const MiningPage = (): React.ReactElement => {
             />      */}
 
             <MiningContainer>
-
-                <div className={confettiStyle}>
-                <Lottie 
-                    ref={lottieRef}
-                    options={defaultOptions}
-                    height={"100%"}
-                    width={"100%"}
-                />
-                </div>
+                {isVisible ? (
+                    <div className={confettiStyle}>
+                        <Lottie ref={lottieRef} options={defaultOptions} height={'100%'} width={'100%'} />
+                    </div>
+                ) : (
+                    <></>
+                )}
 
                 <ContainerAccount>
                     <div className="container">
                         <div className="card">
-                            <div className='coin'>
-                                <Lottie 
-                                    options={defaultOptions2}
-                                    height={"100%"}
-                                    width={"100%"}
-                                />
+                            <div className="coin">
+                                <Lottie options={defaultOptionsCoin} height={'100%'} width={'100%'} />
                             </div>
                             <div className="account">
                                 <AccountTextStyled> Account: {countMining}</AccountTextStyled>
-                            </div>                        
-                            <div className='coin'>
-                                <Lottie 
-                                    options={defaultOptions2}
-                                    height={"100%"}
-                                    width={"100%"}
-                                />
+                            </div>
+                            <div className="coin">
+                                <Lottie options={defaultOptionsCoin} height={'100%'} width={'100%'} />
                             </div>
                         </div>
                     </div>
                 </ContainerAccount>
 
-                {/* <AccountStyle>
-                                    
-                </AccountStyle> */}
-
                 <Speedometer turnAngle={progress} />
 
                 <MotioButton
                     progress={progress}
-                    setProgress={setProgress} 
+                    setProgress={setProgress}
                     countMining={countMining}
                     stepMining={stepMining}
-                    setCountMining={setCountMining} 
+                    setCountMining={setCountMining}
                     stepIncrease={stepIncrease}
                     maxProgress={maxProgress}
                 />
-
-                {isVisible ? (
-                    <div className={containerCenterStyle}>
-                        <Babah/>
-                    </div>
-                    ) : (
-                        <></>
-                    )
-                }
-
             </MiningContainer>
         </>
     );
 };
 
 export default MiningPage;
-
